ee533a11b61b12489a40c2f8a70ee96c620f5348
Yonmoji-X <yametu55@gmail.com>
2024-09-22 20:27:29 +0900

    ナビゲーション整理

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -96,18 +96,46 @@
         </div>
 
             @endif
-            <x-nav-link :href="route('attendances.index')" :active="request()->routeIs('attendances.index')">
+<!-- ==================================================== -->
+
+            <!-- 名簿関連のプルダウンメニュー -->
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <x-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                    <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                        {{ __('勤怠管理') }}
+                        <div class="ms-1">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </button>
+                </x-slot>
+
+                <x-slot name="content">
+                    <x-dropdown-link :href="route('attendances.index')" :active="request()->routeIs('attendances.index')">
+                        {{ __('勤怠管理') }}
+                    </x-dropdown-link>
+
+                    <x-dropdown-link :href="route('breaksessions.index')" :active="request()->routeIs('breaksessions.index')">
+                        {{ __('休憩データ') }}
+                    </x-dropdown-link>
+                </x-slot>
+            </x-dropdown>
+            </div>
+<!-- ==================================================== -->
+            <!-- <x-nav-link :href="route('attendances.index')" :active="request()->routeIs('attendances.index')">
                     {{ __('勤怠管理') }}
-            </x-nav-link>
+            </x-nav-link> -->
             <!-- <x-nav-link :href="route('attendances.create')" :active="request()->routeIs('attendances.create')">
                     {{ __('出退勤(仮)') }}
             </x-nav-link> -->
             <x-nav-link :href="route('records.index')" :active="request()->routeIs('records.index')">
                     {{ __('チェック一覧') }}
             </x-nav-link>
-            <x-nav-link :href="route('breaksessions.index')" :active="request()->routeIs('breaksessions.index')">
+            <!-- <x-nav-link :href="route('breaksessions.index')" :active="request()->routeIs('breaksessions.index')">
                     {{ __('休憩一覧（仮）') }}
-            </x-nav-link>
+            </x-nav-link> -->
             <x-nav-link :href="route('breaksessions.create')" :active="request()->routeIs('breaksessions.create')">
                     {{ __('休憩') }}
             </x-nav-link>
@@ -191,6 +219,9 @@
         @endif
         <x-responsive-nav-link :href="route('attendances.index')" :active="request()->routeIs('attendances.index')">
             {{ __('勤怠管理') }}
+        </x-responsive-nav-link>
+        <x-responsive-nav-link :href="route('breaksessions.index')" :active="request()->routeIs('breaksessions.index')">
+            {{ __('休憩データ') }}
         </x-responsive-nav-link>
         <!-- <x-responsive-nav-link :href="route('attendances.index')" :active="request()->routeIs('attendances.index')">
             {{ __('出退勤（仮）') }}
