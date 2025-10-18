210e8a43260eaa5ac0d06608be25b118630a2917
Yonmoji-X <yametu55@gmail.com>
2025-10-19 02:26:18 +0900

    些細な修正

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -35,6 +35,8 @@
                         <h3 class="text-xl sm:text-2xl font-bold text-gray-900 dark:text-gray-100 mb-2">{{ $plan['name'] }}</h3>
                         <p class="text-gray-600 dark:text-gray-300 mb-4">{{ $plan['price'] }}</p>
                     </div>
+                    <p>これはテストです。</p>
+                    <p>決算しないでください。</p>
 
                     <button 
                         class="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl transition-colors"
