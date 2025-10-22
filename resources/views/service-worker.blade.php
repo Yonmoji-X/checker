d108c7e34df650acb0498259890c80b5f2955eb4
Yonmoji-X <yametu55@gmail.com>
2025-10-22 21:20:49 +0900

    /checker/を削除

--- resources/views/service-worker.blade.php
+++ resources/views/service-worker.blade.php
@@ -8,14 +8,23 @@
     // キャッシュ名とキャッシュするファイルのリスト
     var CACHE_NAME = 'pwa-sample-caches';
     var urlsToCache = [
-        "/checker/",                         // アプリケーションのルート
-        "/checker/index.php",                // メインエントリーポイント（index.php）
+        "/",                         // アプリケーションのルート
+        "/index.php",                // メインエントリーポイント（index.php）
         "{{ asset('css/app.css') }}",       // LaravelでビルドされたCSSファイル
         "{{ asset('js/app.js') }}",         // LaravelでビルドされたJavaScriptファイル
-        "/checker/images/app-icon-192.png", // アイコン画像
-        "/checker/manifest.json",            // マニフェストファイル
-        "/checker/favicon.ico"               // Favicon
+        "/images/app-icon-192.png", // アイコン画像
+        "/manifest.json",            // マニフェストファイル
+        "/favicon.ico"               // Favicon
     ];
+    // var urlsToCache = [
+    //     "/checker/",                         // アプリケーションのルート
+    //     "/checker/index.php",                // メインエントリーポイント（index.php）
+    //     "{{ asset('css/app.css') }}",       // LaravelでビルドされたCSSファイル
+    //     "{{ asset('js/app.js') }}",         // LaravelでビルドされたJavaScriptファイル
+    //     "/checker/images/app-icon-192.png", // アイコン画像
+    //     "/checker/manifest.json",            // マニフェストファイル
+    //     "/checker/favicon.ico"               // Favicon
+    // ];
 
     // インストール処理
     self.addEventListener('install', function(event) {
