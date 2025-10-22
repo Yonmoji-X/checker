84168f8944eb53a3ffbd80e3c246b1db3e8e19b2
Yonmoji-X <yametu55@gmail.com>
2025-10-22 22:11:37 +0900

    ちらつき対策-意味ないかも

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,7 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.body.classList.add('loaded');
+// });
